865e4e03bf05963c48bffe30dbdecf68b2bc6deb
akhilesh427 <jakhilesh427@gmail.com>
2021-08-13 22:10:35 +0530

    NJC file

---
+++ NJCakhilesh.py
@@ -0,0 +1,28 @@
+import sqlite3
+connection=sqlite3.connect('FavMovies.db')
+cursor=connection.cursor()
+
+
+command1=""" CREATE TABLE IF NOT EXISTS FavMovies(name TEXT PRIMARY KEY,actor TEXT,actress TEXT, director TEXT,year INTEGER) """
+
+cursor.execute(command1)
+
+
+cursor.execute(" INSERT INTO FavMovies VALUES ('Sholay','Amitabh Bachchan','Hema Malini','Ramesh Sippy',1975)")
+cursor.execute(" INSERT INTO FavMovies VALUES ('Don','Amitabh Bachchan','Zeenat Aman','Chandra Barot',1978)")
+cursor.execute(" INSERT INTO FavMovies VALUES ('Mimi','Panakj Tripati','Kriti Sanon','Laxman Utekar',2021)")
+cursor.execute(" INSERT INTO FavMovies VALUES ('Inception','LeonardoDeCaprio','Marion Cotillard','Cristopher Nolan',2010)")
+
+
+
+cursor.execute("SELECT * FROM FavMovies")
+results=cursor.fetchall()
+
+
+cursor.execute("SELECT * FROM FavMovies WHERE actor='Amitabh Bachchan' ")
+results2=cursor.fetchall()
+
+#print the results
+print(results)
+
+print(results2)
